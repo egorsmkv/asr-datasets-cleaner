072ded729da63139455f5d2e867d559622a32c05
Yehor Smoliakov <yehors+gh@ukr.net>
2024-07-19 12:54:40 +0300

    A fix

--- download_dataset.py
+++ download_dataset.py
@@ -1,7 +1,9 @@
 from datasets import load_dataset
 
-cache_dir = "./cache2-yodas2-uk/"
+ds1 = load_dataset("espnet/yodas2", "uk000", cache_dir="./cache-yodas2-uk/")
 
-ds = load_dataset("espnet/yodas2", "uk100", cache_dir=cache_dir)
+print("First dataset is downloaded")
 
-print("Finished")
+ds2 = load_dataset("espnet/yodas2", "uk100", cache_dir= "./cache2-yodas2-uk/")
+
+print("Second dataset is downloaded")
