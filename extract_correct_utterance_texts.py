f65ce0ecdfee64ba01e02956905fb7041bc69149
Yehor Smoliakov <yehors+gh@ukr.net>
2024-07-23 16:35:00 +0300

    Add `--add_utt_ids` flag

--- extract_correct_utterance_texts.py
+++ extract_correct_utterance_texts.py
@@ -3,6 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Extract correct utterance texts")
 
+parser.add_argument("-a", "--add_utt_ids", default='no', required=False)
 parser.add_argument("-f", "--file", required=True)
 parser.add_argument("-t", "--to", required=True)
 
@@ -16,11 +17,15 @@ with open(args.file, "r") as f:
 with open(args.to, "w") as f_to:
     for jsonline in jsonlines:
         texts = jsonline["utterances"]["text"]
+        utt_ids = jsonline["utterances"]["utt_id"]
 
         correct_texts = []
         for idx, text in enumerate(texts):
             if len(text) == 0:
                 continue
+            if args.add_utt_ids == 'yes':
+                utt_id = utt_ids[idx]
+                text = f'{utt_id}\t{text}'
             correct_texts.append(text)
 
         for text in correct_texts:
